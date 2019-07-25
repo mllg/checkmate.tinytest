23649a87fb7bb965c3f6f4789d2bff77fe76ba30
Michel Lang <michellang@gmail.com>
2019-07-25 13:53:36 +0200

    cleanup

--- R/expectations.R
+++ R/expectations.R
@@ -1,3 +1,34 @@
+# 1: name of x
+# 2: name of function to call
+# 3: function arguments, as collapsed string
+template = '{
+    res = %2$s(%3$s)
+
+    tinytest::tinytest(
+      result = isTRUE(res),
+      call = sys.call(sys.parent(1)),
+      diff = if (is.character(res)) res else "",
+      short = %1$s
+    )
+}'
+
+as_expectation = function(check.fun, env = parent.frame()) {
+  fun.name = if (!is.character(check.fun)) deparse(substitute(check.fun)) else check.fun
+  check.fun = match.fun(check.fun)
+  fun.args = formals(args(check.fun))
+  x.name = names(fun.args[1L])
+  x = NULL
+  body = sprintf(template, x.name, fun.name, paste0(names(fun.args), collapse = ", "))
+
+  new.fun = function() TRUE
+  formals(new.fun) = fun.args
+  body(new.fun) = parse(text = body)
+  environment(new.fun) = env
+
+  return(new.fun)
+}
+
+
 #' @title Expectations for 'tinytest'
 #'
 #' @description
@@ -5,180 +36,179 @@
 #' See the respective help page in \CRANpkg{checkmate} (replace `expect_` with `check_`) for a
 #' description of the function arguments.
 #'
-#' @include makeExpectation.R
 #' @keywords internal
 #' @name expect_tinytest
 NULL
 
 #' @rdname expect_tinytest
 #' @export
-expect_access = makeExpectationFunction(checkmate::check_access)
+expect_access = as_expectation(checkmate::check_access)
 
 #' @rdname expect_tinytest
 #' @export
-expect_array = makeExpectationFunction(checkmate::check_array)
+expect_array = as_expectation(checkmate::check_array)
 
 #' @rdname expect_tinytest
 #' @export
-expect_atomic = makeExpectationFunction(checkmate::check_atomic)
+expect_atomic = as_expectation(checkmate::check_atomic)
 
 #' @rdname expect_tinytest
 #' @export
-expect_atomic_vector = makeExpectationFunction(checkmate::check_atomic_vector)
+expect_atomic_vector = as_expectation(checkmate::check_atomic_vector)
 
 #' @rdname expect_tinytest
 #' @export
-expect_bit = makeExpectationFunction(checkmate::check_bit)
+expect_bit = as_expectation(checkmate::check_bit)
 
 #' @rdname expect_tinytest
 #' @export
-expect_character = makeExpectationFunction(checkmate::check_character)
+expect_character = as_expectation(checkmate::check_character)
 
 #' @rdname expect_tinytest
 #' @export
-expect_choice = makeExpectationFunction(checkmate::check_choice)
+expect_choice = as_expectation(checkmate::check_choice)
 
 #' @rdname expect_tinytest
 #' @export
-expect_class = makeExpectationFunction(checkmate::check_class)
+expect_class = as_expectation(checkmate::check_class)
 
 #' @rdname expect_tinytest
 #' @export
-expect_complex = makeExpectationFunction(checkmate::check_complex)
+expect_complex = as_expectation(checkmate::check_complex)
 
 #' @rdname expect_tinytest
 #' @export
-expect_count = makeExpectationFunction(checkmate::check_count)
+expect_count = as_expectation(checkmate::check_count)
 
 #' @rdname expect_tinytest
 #' @export
-expect_data_frame = makeExpectationFunction(checkmate::check_data_frame)
+expect_data_frame = as_expectation(checkmate::check_data_frame)
 
 #' @rdname expect_tinytest
 #' @export
-expect_data_table = makeExpectationFunction(checkmate::check_data_table)
+expect_data_table = as_expectation(checkmate::check_data_table)
 
 #' @rdname expect_tinytest
 #' @export
-expect_date = makeExpectationFunction(checkmate::check_date)
+expect_date = as_expectation(checkmate::check_date)
 
 #' @rdname expect_tinytest
 #' @export
-expect_directory_exists = makeExpectationFunction(checkmate::check_directory_exists)
+expect_directory_exists = as_expectation(checkmate::check_directory_exists)
 
 #' @rdname expect_tinytest
 #' @export
-expect_disjunct = makeExpectationFunction(checkmate::check_disjunct)
+expect_disjunct = as_expectation(checkmate::check_disjunct)
 
 #' @rdname expect_tinytest
 #' @export
-expect_double = makeExpectationFunction(checkmate::check_double)
+expect_double = as_expectation(checkmate::check_double)
 
 #' @rdname expect_tinytest
 #' @export
-expect_environment = makeExpectationFunction(checkmate::check_environment)
+expect_environment = as_expectation(checkmate::check_environment)
 
 
 #' @rdname expect_tinytest
 #' @export
-expect_factor = makeExpectationFunction(checkmate::check_factor)
+expect_factor = as_expectation(checkmate::check_factor)
 
 #' @rdname expect_tinytest
 #' @export
-expect_file_exists = makeExpectationFunction(checkmate::check_file_exists)
+expect_file_exists = as_expectation(checkmate::check_file_exists)
 
 #' @rdname expect_tinytest
 #' @export
-expect_flag = makeExpectationFunction(checkmate::check_flag)
+expect_flag = as_expectation(checkmate::check_flag)
 
 #' @rdname expect_tinytest
 #' @export
-expect_formula = makeExpectationFunction(checkmate::check_formula)
+expect_formula = as_expectation(checkmate::check_formula)
 
 #' @rdname expect_tinytest
 #' @export
-expect_function = makeExpectationFunction(checkmate::check_function)
+expect_function = as_expectation(checkmate::check_function)
 
 #' @rdname expect_tinytest
 #' @export
-expect_int = makeExpectationFunction(checkmate::check_int)
+expect_int = as_expectation(checkmate::check_int)
 
 #' @rdname expect_tinytest
 #' @export
-expect_integer = makeExpectationFunction(checkmate::check_integer)
+expect_integer = as_expectation(checkmate::check_integer)
 
 #' @rdname expect_tinytest
 #' @export
-expect_integerish = makeExpectationFunction(checkmate::check_integerish)
+expect_integerish = as_expectation(checkmate::check_integerish)
 
 #' @rdname expect_tinytest
 #' @export
-expect_list = makeExpectationFunction(checkmate::check_list)
+expect_list = as_expectation(checkmate::check_list)
 
 #' @rdname expect_tinytest
 #' @export
-expect_logical = makeExpectationFunction(checkmate::check_logical)
+expect_logical = as_expectation(checkmate::check_logical)
 
 #' @rdname expect_tinytest
 #' @export
-expect_matrix = makeExpectationFunction(checkmate::check_matrix)
+expect_matrix = as_expectation(checkmate::check_matrix)
 
 #' @rdname expect_tinytest
 #' @export
-expect_multi_class = makeExpectationFunction(checkmate::check_multi_class)
+expect_multi_class = as_expectation(checkmate::check_multi_class)
 
 #' @rdname expect_tinytest
 #' @export
-expect_named = makeExpectationFunction(checkmate::check_named)
+expect_named = as_expectation(checkmate::check_named)
 
 #' @rdname expect_tinytest
 #' @export
-expect_names = makeExpectationFunction(checkmate::check_names)
+expect_names = as_expectation(checkmate::check_names)
 
 #' @rdname expect_tinytest
 #' @export
-expect_number = makeExpectationFunction(checkmate::check_number)
+expect_number = as_expectation(checkmate::check_number)
 
 #' @rdname expect_tinytest
 #' @export
-expect_numeric = makeExpectationFunction(checkmate::check_numeric)
+expect_numeric = as_expectation(checkmate::check_numeric)
 
 #' @rdname expect_tinytest
 #' @export
-expect_path_for_output = makeExpectationFunction(checkmate::check_path_for_output)
+expect_path_for_output = as_expectation(checkmate::check_path_for_output)
 
 #' @rdname expect_tinytest
 #' @export
-expect_posixct = makeExpectationFunction(checkmate::check_posixct)
+expect_posixct = as_expectation(checkmate::check_posixct)
 
 #' @rdname expect_tinytest
 #' @export
-expect_r6 = makeExpectationFunction(checkmate::check_r6)
+expect_r6 = as_expectation(checkmate::check_r6)
 
 #' @rdname expect_tinytest
 #' @export
-expect_raw = makeExpectationFunction(checkmate::check_raw)
+expect_raw = as_expectation(checkmate::check_raw)
 
 #' @rdname expect_tinytest
 #' @export
-expect_scalar = makeExpectationFunction(checkmate::check_scalar)
+expect_scalar = as_expectation(checkmate::check_scalar)
 
 #' @rdname expect_tinytest
 #' @export
-expect_scalar_na = makeExpectationFunction(checkmate::check_scalar_na)
+expect_scalar_na = as_expectation(checkmate::check_scalar_na)
 
 #' @rdname expect_tinytest
 #' @export
-expect_set_equal = makeExpectationFunction(checkmate::check_set_equal)
+expect_set_equal = as_expectation(checkmate::check_set_equal)
 
 #' @rdname expect_tinytest
 #' @export
-expect_string = makeExpectationFunction(checkmate::check_string)
+expect_string = as_expectation(checkmate::check_string)
 
 #' @rdname expect_tinytest
 #' @export
-expect_subset = makeExpectationFunction(checkmate::check_subset)
+expect_subset = as_expectation(checkmate::check_subset)
 
 #' @rdname expect_tinytest
 #' @export
-expect_tibble = makeExpectationFunction(checkmate::check_tibble)
+expect_tibble = as_expectation(checkmate::check_tibble)
