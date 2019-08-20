4a09a5b598a20697dc6279064d3da14e7cdd5530
Michel Lang <michellang@gmail.com>
2019-08-20 08:33:31 +0200

    remove onUnload

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,3 @@
   expectations = names(ns)[grepl("^expect_", names(ns))]
   tinytest::register_tinytest_extension(pkgname, expectations)
 }
-
-# .onUnload = function(libpath) {
-#   tinytest::unregister_tinytest_extension("checkmate.tinytest")
-# }
